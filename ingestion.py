d4f449f4e190176e15e60540d6a628285fc735b4
Nikeshh <nikeshhbaskaran01@gmail.com>
2024-07-06 23:55:52 -0400

    Ingestion, LLM and Streamlit implementation

--- ingestion.py
+++ ingestion.py
@@ -1,33 +1,38 @@
+import os
 from dotenv import load_dotenv
+from langchain_community.document_loaders.readthedocs import ReadTheDocsLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores.pinecone import Pinecone as langchainPinecone
+from pinecone import Pinecone
+from consts import INDEX_NAME
 
 load_dotenv()
 
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.document_loaders import ReadTheDocsLoader
-from langchain_openai import OpenAIEmbeddings
-from langchain_pinecone import PineconeVectorStore
+pc = Pinecone(
+    api_key=os.environ.get("PINECONE_API_KEY"),
+    environment=os.environ["PINECONE_ENVIRONMENT_REGION"],
+)
 
-embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
-
-
-def ingest_docs():
-    loader = ReadTheDocsLoader("langchain-docs/langchain.readthedocs.io/en/v0.1")
-
+def ingest_docs() -> None:
+    loader = ReadTheDocsLoader(path="langchain-docs")
     raw_documents = loader.load()
-    print(f"loaded {len(raw_documents)} documents")
+    print(f"loaded {len(raw_documents) }documents")
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000, chunk_overlap=100, separators=["\n\n", "\n", " ", ""]
+    )
+    documents = text_splitter.split_documents(documents=raw_documents)
+    print(f"Splitted into {len(documents)} chunks")
 
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=600, chunk_overlap=50)
-    documents = text_splitter.split_documents(raw_documents)
     for doc in documents:
-        new_url = doc.metadata["source"]
-        new_url = new_url.replace("langchain-docs", "https:/")
+        old_path = doc.metadata["source"]
+        new_url = old_path.replace("langchain-docs", "https:/")
         doc.metadata.update({"source": new_url})
 
-    print(f"Going to add {len(documents)} to Pinecone")
-    PineconeVectorStore.from_documents(
-        documents, embeddings, index_name="langchain-doc-index"
-    )
-    print("****Loading to vectorstore done ***")
+    print(f"Going to insert {len(documents)} to Pinecone")
+    embeddings = OpenAIEmbeddings()
+    langchainPinecone.from_documents(documents, embeddings, index_name=INDEX_NAME)
+    print("****** Added to Pinecone vectorstore vectors")
 
 
 if __name__ == "__main__":
